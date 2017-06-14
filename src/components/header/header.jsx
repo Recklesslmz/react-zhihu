09474ce9b0cb57df4f2fe137a4a8804f388998cf
Recklesslmz <936838918@qq.com>
2017-06-14 22:52:01 +0800

    fixed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
                 <div  className='avatar'><img src='http://om10u3y7b.bkt.clouddn.com/WechatIMG830.jpeg'></img></div>
                 <div className='name'>
                     <div className="memberName" onClick={this.onChanges}>李木子React</div>
-                    <div className="memberDesc">查看个人主页或编辑界面</div>
+                    <div className="memberDesc">主页或编辑界面</div>
                 </div>
 
             </div>
